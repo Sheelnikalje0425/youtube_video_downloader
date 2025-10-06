294da82e322564b104ea9c9abc3afca7201ea6cf
sheelnikalje25 <sheelnikalje2223@ternaengg.ac.in>
2025-10-06 13:18:19 +0530

    dev branch

--- app.py
+++ app.py
@@ -11,13 +11,32 @@ def index():
 @app.route('/download', methods=['POST'])
 def download():
     url = request.form['url']
+    file_format = request.form['format']  # Get the selected format
     download_path = os.path.join(os.path.expanduser("~"), "Downloads")
-    ydl_opts = {'outtmpl': os.path.join(download_path, '%(title)s.%(ext)s')}
 
-    with YoutubeDL(ydl_opts) as ydl:
-        ydl.download([url])
+    if file_format == "mp3":
+        ydl_opts = {
+            'format': 'bestaudio/best',
+            'outtmpl': os.path.join(download_path, '%(title)s.%(ext)s'),
+            'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'mp3',
+                'preferredquality': '192',
+            }],
+        }
+    else:
+        ydl_opts = {
+            'format': 'best',
+            'outtmpl': os.path.join(download_path, '%(title)s.%(ext)s'),
+        }
 
-    return render_template('index.html', success=True)
+    try:
+        with YoutubeDL(ydl_opts) as ydl:
+            ydl.download([url])
+        return render_template('index.html', success=True)
+    except Exception as e:
+        print("Error:", e)
+        return render_template('index.html', success=False)
 
 if __name__ == '__main__':
     app.run(debug=True)
